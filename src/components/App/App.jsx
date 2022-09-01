aed236061177bf6a9c5cfac331ea2babec07ed0c
leopard-work <zobnin.vladislav@yandex.ru>
2022-09-02 00:14:51 +0300

    + data.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,21 +5,21 @@ import "@ya.praktikum/react-developer-burger-ui-components";
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
-function App() {
-  return (
-      <>
+import {data} from '../../utils/data';
 
+function App() {
+    return (
+      <>
           <AppHeader />
           <main className="pb-10">
             <div className="container pl-4 pr-4">
                 <div className="main_blocks">
-                    <BurgerIngredients />
+                    <BurgerIngredients data={data} />
                 </div>
             </div>
           </main>
-
       </>
-  );
+    );
 }
 
 export default App;
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -22,99 +22,60 @@ const TabsNav = () => {
     )
 }
 
-const Tabs = () => {
+const Tabs = (props) => {
     return (
         <div className={headerStyles.content }>
             <div className={headerStyles.content_block+ " mt-10"}>
                 <h2 className="text text_type_main-medium">Булки</h2>
-                <ul className={headerStyles.items + " pl-4 pr-4"}>
-                    <li className={headerStyles.item + " mt-6"}>
-                        <div className={headerStyles.item_image + " ml-4 mr-4"}>
-                            <img src="https://code.s3.yandex.net/react/code/cheese.png" alt="" />
-                        </div>
-                        <div className={headerStyles.item_price + " mt-1 mb-1"}>
-                            <span className="text text_type_digits-default mr-2">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <h3 className={headerStyles.item_title + " p-1 text text_type_main-default"}>Краторная булка N-200i</h3>
-                        <Counter count={1} size="default" />
-                    </li>
-                    <li className={headerStyles.item + " mt-6"}>
-                        <div className={headerStyles.item_image + " ml-4 mr-4"}>
-                            <img src="https://code.s3.yandex.net/react/code/cheese.png" alt="" />
-                        </div>
-                        <div className={headerStyles.item_price + " mt-1 mb-1"}>
-                            <span className="text text_type_digits-default mr-2">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <h3 className={headerStyles.item_title + " p-1 text text_type_main-default"}>Краторная булка N-200i</h3>
-                    </li>
-                </ul>
+                {<TabsCategory category="bun" data={props.data} />}
             </div>
             <div className={headerStyles.content_block+ " mt-10"}>
-                <h2 className="text text_type_main-medium">Булки</h2>
-                <ul className={headerStyles.items + " pl-4 pr-4"}>
-                    <li className={headerStyles.item + " mt-6"}>
-                        <div className={headerStyles.item_image + " ml-4 mr-4"}>
-                            <img src="https://code.s3.yandex.net/react/code/cheese.png" alt="" />
-                        </div>
-                        <div className={headerStyles.item_price + " mt-1 mb-1"}>
-                            <span className="text text_type_digits-default mr-2">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <h3 className={headerStyles.item_title + " p-1 text text_type_main-default"}>Краторная булка N-200i</h3>
-                        <Counter count={1} size="default" />
-                    </li>
-                    <li className={headerStyles.item + " mt-6"}>
-                        <div className={headerStyles.item_image + " ml-4 mr-4"}>
-                            <img src="https://code.s3.yandex.net/react/code/cheese.png" alt="" />
-                        </div>
-                        <div className={headerStyles.item_price + " mt-1 mb-1"}>
-                            <span className="text text_type_digits-default mr-2">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <h3 className={headerStyles.item_title + " p-1 text text_type_main-default"}>Краторная булка N-200i</h3>
-                    </li>
-                </ul>
+                <h2 className="text text_type_main-medium">Соусы</h2>
+                {<TabsCategory category="sauce" data={props.data} />}
             </div>
             <div className={headerStyles.content_block+ " mt-10"}>
-                <h2 className="text text_type_main-medium">Булки</h2>
-                <ul className={headerStyles.items + " pl-4 pr-4"}>
-                    <li className={headerStyles.item + " mt-6"}>
-                        <div className={headerStyles.item_image + " ml-4 mr-4"}>
-                            <img src="https://code.s3.yandex.net/react/code/cheese.png" alt="" />
-                        </div>
-                        <div className={headerStyles.item_price + " mt-1 mb-1"}>
-                            <span className="text text_type_digits-default mr-2">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <h3 className={headerStyles.item_title + " p-1 text text_type_main-default"}>Краторная булка N-200i</h3>
-                        <Counter count={1} size="default" />
-                    </li>
-                    <li className={headerStyles.item + " mt-6"}>
-                        <div className={headerStyles.item_image + " ml-4 mr-4"}>
-                            <img src="https://code.s3.yandex.net/react/code/cheese.png" alt="" />
-                        </div>
-                        <div className={headerStyles.item_price + " mt-1 mb-1"}>
-                            <span className="text text_type_digits-default mr-2">20</span>
-                            <CurrencyIcon type="primary" />
-                        </div>
-                        <h3 className={headerStyles.item_title + " p-1 text text_type_main-default"}>Краторная булка N-200i</h3>
-                    </li>
-                </ul>
+                <h2 className="text text_type_main-medium">Начинки</h2>
+                {<TabsCategory category="main" data={props.data} />}
             </div>
         </div>
     )
 }
 
-function BurgerIngredients() {
-  return (
+const TabsCategory = (props) => {
+    const items = props.data.filter(function(category) {
+        return category.type === props.category;
+    });
+    return (
+        <ul className={headerStyles.items + " pl-4 pr-4"}>
+            {items.map(item => <TabsItem item={item} />)}
+        </ul>
+    )
+}
+
+const TabsItem = (props) => {
+    return (
+        <li className={headerStyles.item + " mt-6"}>
+            <div className={headerStyles.item_image + " ml-4 mr-4"}>
+                <img src="https://code.s3.yandex.net/react/code/cheese.png" alt="" />
+            </div>
+            <div className={headerStyles.item_price + " mt-1 mb-1"}>
+                <span className="text text_type_digits-default mr-2">20</span>
+                <CurrencyIcon type="primary" />
+            </div>
+            <h3 className={headerStyles.item_title + " p-1 text text_type_main-default"}>{props.item.name}</h3>
+            <Counter count={1} size="default" />
+        </li>
+    )
+}
+
+function BurgerIngredients(props) {
+    return (
       <section className={headerStyles.section + " mt-10"}>
           <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
           {<TabsNav />}
-          {<Tabs />}
+          {<Tabs data={props.data} />}
       </section>
-  );
+    );
 }
 
 export default BurgerIngredients;

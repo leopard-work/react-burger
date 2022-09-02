9012a06cf961b025174767227cff4447c3651d38
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2022-09-02 16:10:44 +0300

    app fix

--- src/components/app1/app.jsx
+++ src/components/app1/app.jsx
@@ -6,15 +6,17 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
+import styles from './app.module.css';
+
 import {data} from '../../utils/data';
 
 function App() {
     return (
       <>
           <AppHeader />
-          <main className="pb-10">
+          <main className={styles.main + " pb-10"}>
             <div className="container pl-4 pr-4">
-                <div className="main_blocks">
+                <div className={styles.blocks}>
                     <BurgerIngredients data={data} />
                     <BurgerConstructor data={data} />
                 </div>

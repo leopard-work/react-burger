47c914fb1ecc20b8bb0499cf47927f063d101c6f
Зобнин Владислав <zobnin.vladislav@yandex.ru>
2022-11-23 15:52:34 +0300

    + информации о заказе

--- src/components/orders/orders.tsx
+++ src/components/orders/orders.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
 import "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./orders.module.css";
+import stylesItem from "./orders_item.module.css";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "../../services/reducers";
+import { ItemProps } from "../../utils/types";
+import Modal from "../modal/modal";
 
 export const Orders = () => {
   return (
@@ -46,6 +51,111 @@ export const OrdersItems = () => {
           <CurrencyIcon type="primary" />
         </div>
       </div>
+      <OrdersModal />
     </div>
+  );
+};
+
+export const OrderItem = () => {
+  return (
+    <div className={stylesItem.wrapper + " mt-10 mb-10"}>
+      <p className="text text_type_digits-default mb-10">#034533</p>
+      <p className="text text_type_main-medium mb-3">
+        Death Star Starship Main бургер
+      </p>
+      <p className="text text_type_main-default mb-15">Создан</p>
+      <p className="text text_type_main-medium mb-6">Состав:</p>
+      <div className={stylesItem.params + " pr-6 mb-10"}>
+        <div className={stylesItem.param + " mb-4"}>
+          <div className={stylesItem.title}>
+            <div className={stylesItem.icon + " mr-4"}></div>
+            <p className="text text_type_main-default">
+              Флюоресцентная булка R2-D3
+            </p>
+          </div>
+          <div className={styles.price}>
+            <p className="text text_type_digits-default mr-2">480</p>{" "}
+            <CurrencyIcon type="primary" />
+          </div>
+        </div>
+        <div className={stylesItem.param + " mb-4"}>
+          <div className={stylesItem.title}>
+            <div className={stylesItem.icon + " mr-4"}></div>
+            <p className="text text_type_main-default">
+              Флюоресцентная булка R2-D3
+            </p>
+          </div>
+          <div className={styles.price}>
+            <p className="text text_type_digits-default mr-2">2 x 480</p>{" "}
+            <CurrencyIcon type="primary" />
+          </div>
+        </div>
+        <div className={stylesItem.param + " mb-4"}>
+          <div className={stylesItem.title}>
+            <div className={stylesItem.icon + " mr-4"}></div>
+            <p className="text text_type_main-default">
+              Флюоресцентная булка R2-D3
+            </p>
+          </div>
+          <div className={styles.price}>
+            <p className="text text_type_digits-default mr-2">480</p>{" "}
+            <CurrencyIcon type="primary" />
+          </div>
+        </div>
+        <div className={stylesItem.param + " mb-4"}>
+          <div className={stylesItem.title}>
+            <div className={stylesItem.icon + " mr-4"}></div>
+            <p className="text text_type_main-default">
+              Флюоресцентная булка R2-D3
+            </p>
+          </div>
+          <div className={styles.price}>
+            <p className="text text_type_digits-default mr-2">480</p>{" "}
+            <CurrencyIcon type="primary" />
+          </div>
+        </div>
+        <div className={stylesItem.param + " mb-4"}>
+          <div className={stylesItem.title}>
+            <div className={stylesItem.icon + " mr-4"}></div>
+            <p className="text text_type_main-default">
+              Флюоресцентная булка R2-D3
+            </p>
+          </div>
+          <div className={styles.price}>
+            <p className="text text_type_digits-default mr-2">1 x 480</p>{" "}
+            <CurrencyIcon type="primary" />
+          </div>
+        </div>
+      </div>
+      <div className={stylesItem.bottom}>
+        <p className="text text_type_main-default text_color_inactive">
+          Сегодня, 16:20 i-GMT+3
+        </p>
+        <div className={styles.price}>
+          <p className="text text_type_digits-default mr-2">580</p>{" "}
+          <CurrencyIcon type="primary" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const OrdersModal = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  /*const openOrder = (order: any) => {
+      dispatch({ type: VIEWORDER_OPEN, item: order });
+      history.replace(`/orders/${order._id}`, { modal: true });
+    };*/
+
+  const modalClose = () => {
+    //dispatch({ type: CLOSE_VIEW_ITEM });
+    history.push("/feed");
+  };
+  return (
+    <Modal isOpen={true} close={() => modalClose()}>
+      <OrderItem />
+    </Modal>
   );
 };

e32702c9049fb0f8499a21223b6e15932ab8a8ef
leopard-work <zobnin.vladislav@yandex.ru>
2022-11-27 21:24:57 +0300

    + удален лишний блок

--- src/index.tsx
+++ src/index.tsx
@@ -34,18 +34,6 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-// declare global {
-//   interface Window {
-//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//   }
-// }
-//
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const enhancer = composeEnhancers(
-//   applyMiddleware(thunk, socketMiddleware(ordersWsActions))
-// );
-// export const store = createStore(rootReducer, enhancer);
-
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
